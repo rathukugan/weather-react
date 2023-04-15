2a5e17e00ff7a03bf91dc726faf3684a8bcf1b47
rath <rathu.kugan@gmail.com>
2023-04-14 22:52:52 -0400

    Added materialui and search bar to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Search from './components/search.js'
+
 //{process.env.REACT_APP_API_URL}
 //{process.env.REACT_APP_API_KEY}
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+
 function App() {
   return (
     <div className="App">
-
+      <h1>Weather</h1>
+      <Search></Search>
     </div>
   );
 }
---
+++ src/components/search.js
@@ -0,0 +1,9 @@
+import TextField from '@mui/material/TextField';
+
+function Search() {
+    return (
+        <TextField id="city-search" label="City" variant="outlined" />
+    );
+  }
+
+  export default Search;
